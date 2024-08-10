f2379fe4228f1a8452e386829ad4fe7c3326d29d
bcnsrui <bcnsuri@naver.com>
2024-08-10 21:57:08 +0900

    Update CS_init.lua

--- script/CS_init.lua
+++ script/CS_init.lua
@@ -1,5 +1,15 @@
 LOCATION_EMZONE = 0x1000
 
+function Duel.IsMainPhase()
+	local phase=Duel.GetCurrentPhase()
+	return phase==PHASE_MAIN1 or phase==PHASE_MAIN2
+end
+
+function Duel.IsBattlePhase()
+	local phase=Duel.GetCurrentPhase()
+	return phase>=PHASE_BATTLE_START and phase<=PHASE_BATTLE
+end
+
 --Kirafan TCG Utility
 Duel.LoadScript("KirafanMainCharacter.lua")
 Duel.LoadScript("KirafanCreamate.lua")
