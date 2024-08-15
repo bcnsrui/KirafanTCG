2e3eabf590d2e3b44fd145c142d4b9a50894b26d
bcnsrui <bcnsuri@naver.com>
2024-08-15 10:19:22 +0900

    Update c10052131.lua

--- script/c10052131.lua
+++ script/c10052131.lua
@@ -7,7 +7,7 @@ function s.initial_effect(c)
 	e5:SetCode(EVENT_FREE_CHAIN)
 	e5:SetRange(LOCATION_MZONE)
 	e5:SetCondition(s.damcon)
-	e5:SetCost(Kirafan2.dottecost4)
+	e5:SetCost(s.dottecost4)
 	e5:SetTarget(s.damtg)
 	e5:SetOperation(s.damop)
 	c:RegisterEffect(e5)
@@ -25,6 +25,7 @@ end
 function s.damcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetCurrentPhase()==PHASE_BATTLE_STEP and e:GetHandler():GetCounter(0xb03)==0 and not e:GetHandler():IsDefensePos()
 	and not (Duel.GetAttacker() and Duel.GetAttacker():IsControler(tp)) and Duel.GetTurnPlayer()==tp
+	and Duel.IsExistingMatchingCard(s.battlefilter,tp,LOCATION_MZONE,0,2,e:GetHandler())
 	and Duel.IsExistingMatchingCard(Card.IsAbleToRemoveAsCost,tp,LOCATION_GRAVE,0,4,nil,tp)
 end
 function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -41,7 +42,7 @@ end
 function s.damop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local enemy=Duel.GetMatchingGroup(Kirafan.NoEmzonefilter,tp,0,LOCATION_MZONE,nil)
-	local ally=Duel.GetMatchingGroup(Kirafan3.Nohealfilter,tp,LOCATION_MZONE,0,nil)
+	local ally=Duel.GetMatchingGroup(Kirafan.NoEmzonefilter,tp,LOCATION_MZONE,0,nil)
 	local dam=1
 	Duel.Damage(1-tp,dam,REASON_EFFECT)
 	local ag=enemy:GetFirst()
@@ -52,10 +53,6 @@ function s.damop(e,tp,eg,ep,ev,re,r,rp)
 	tc=ag:GetOverlayGroup():RandomSelect(1-tp,dam)
 	Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
 	end	end
-	if Duel.IsExistingMatchingCard(s.battlefilter,tp,LOCATION_MZONE,0,2,c)
-	and Duel.SelectYesNo(tp,aux.Stringid(10050111,7)) then
-	local ag2=Duel.SelectMatchingCard(tp,s.battlefilter,tp,LOCATION_MZONE,0,2,2,c)
-	Duel.ChangePosition(ag2,POS_FACEUP_DEFENSE)
 	local bg=ally:GetFirst()
 	for bg in aux.Next(ally) do
 	local e1=Effect.CreateEffect(c)
@@ -63,5 +60,45 @@ function s.damop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetCode(EFFECT_EXTRA_ATTACK)
 	e1:SetValue(1)
 	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-	bg:RegisterEffect(e1) end end
+	bg:RegisterEffect(e1) end
+end
+
+function s.dottecost4(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.IsExistingMatchingCard(s.battlefilter,tp,LOCATION_MZONE,0,2,c)
+	and Duel.IsExistingMatchingCard(Card.IsAbleToRemoveAsCost,tp,LOCATION_GRAVE,0,4,nil) end
+	
+	local g=Duel.GetMatchingGroup(Card.IsAbleToRemoveAsCost,tp,LOCATION_GRAVE,0,nil)
+	local last=g:GetFirst()
+	local tc=g:GetNext()
+	for tc in aux.Next(g) do
+		if tc:GetSequence()<last:GetSequence() then last=tc end
+	end
+	Duel.Remove(last,POS_FACEUP,REASON_EFFECT)
+	local g2=Duel.GetMatchingGroup(Card.IsAbleToRemoveAsCost,tp,LOCATION_GRAVE,0,nil)
+	local last2=g2:GetFirst()
+	local tc2=g2:GetNext()
+	for tc2 in aux.Next(g2) do
+		if tc2:GetSequence()<last2:GetSequence() then last2=tc2 end
+	end
+	Duel.Remove(last2,POS_FACEUP,REASON_EFFECT)
+	local g3=Duel.GetMatchingGroup(Card.IsAbleToRemoveAsCost,tp,LOCATION_GRAVE,0,nil)
+	local last3=g3:GetFirst()
+	local tc3=g3:GetNext()
+	for tc3 in aux.Next(g3) do
+		if tc3:GetSequence()<last3:GetSequence() then last3=tc3 end
+	end
+	Duel.Remove(last3,POS_FACEUP,REASON_EFFECT)
+	local g4=Duel.GetMatchingGroup(Card.IsAbleToRemoveAsCost,tp,LOCATION_GRAVE,0,nil)
+	local last4=g4:GetFirst()
+	local tc4=g4:GetNext()
+	for tc4 in aux.Next(g4) do
+		if tc4:GetSequence()<last4:GetSequence() then last4=tc4 end
+	end
+	Duel.Remove(last4,POS_FACEUP,REASON_EFFECT)
+	
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(10050115,0))
+	Duel.ChangePosition(c,POS_FACEUP_DEFENSE)
+	local ag=Duel.SelectMatchingCard(tp,s.battlefilter,tp,LOCATION_MZONE,0,2,2,c)
+	Duel.ChangePosition(ag,POS_FACEUP_DEFENSE)
 end
